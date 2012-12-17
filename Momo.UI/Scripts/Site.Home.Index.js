0bd67c0c4234436eb10846c66d46df5de37cf8c3
Rob Tennyson <rob@robtennyson.us>
2012-12-17 12:32:44 -0600

    Start of home page

---
+++ Momo.UI/Scripts/Site.Home.Index.js
@@ -0,0 +1,9 @@
+﻿$(function() {
+
+    var module = app.modules.homeIndex = {
+        shoppingLists: $('#shoppingLists')
+    };
+
+    setTimeout(function () { module.shoppingLists.text('woot'); }, 1000);
+
+});
