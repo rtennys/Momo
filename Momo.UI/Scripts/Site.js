3fce984edae2d327161e9343107b497b3cb5456b
Rob Tennyson <rob@robtennyson.us>
2012-12-17 11:45:24 -0600

    Added site script and smooth scrolling

---
+++ Momo.UI/Scripts/Site.js
@@ -0,0 +1,155 @@
+﻿/******************************************************/
+/******************************************************/
+// jquery extensions
+
+$.fn.setToSizeOfWindow = function () {
+    return this.each(function () {
+        $(this).css({ width: $(window).width(), height: $(document).height() });
+    });
+};
+
+$.fn.centerInScreen = function (width, height) {
+    width = width || '85%';
+    height = height || '85%';
+    return this.each(function () {
+        var element = $(this);
+        element.css({ width: width, height: height });
+        var top = ($(window).height() - element.outerHeight()) / 2;
+        var left = ($(window).width() - element.outerWidth()) / 2;
+        element.css({ top: top, left: left });
+    });
+};
+
+$.fn.resetUnobtrusiveValidation = function () {
+    return this.each(function () {
+        var form = $(this);
+        form.removeData('validator');
+        form.removeData('unobtrusiveValidation');
+        form.find('[data-valmsg-summary=true]').addClass('validation-summary-valid').removeClass('validation-summary-errors').find('ul').empty();
+        $.validator.unobtrusive.parse(form);
+    });
+};
+
+$.fn.appendValidationErrors = function (errors) {
+    return this.each(function () {
+        var container = $(this).find('[data-valmsg-summary=true]'), list = container.find('ul');
+        container.addClass('validation-summary-errors').removeClass('validation-summary-valid');
+        $.each(errors, function (i, val) {
+            $('<li />').html(val).appendTo(list);
+        });
+    });
+};
+
+$.fn.toObject = function () {
+    /// <summary>Only call on form elements or form input elements</summary>
+
+    var obj = {};
+
+    this.each(function () {
+        $.map($(this).serializeArray(), function (n) {
+            obj[n.name] = n.value;
+        });
+    });
+
+    return obj;
+};
+
+
+
+/******************************************************/
+/******************************************************/
+// application namespace
+
+app = {
+    webroot: '/', // override in _Layout for dynamic web root
+
+    modules: {}, // add modules here for consistency and convenience in the debugger
+
+    debug: function(message) {
+        if (window.console && 'debug' in window.console)
+            console.debug(message);
+        else if (window.console && 'log' in window.console)
+            console.log(message);
+        else
+            $('<div>').text(message).appendTo('body');
+        return this;
+    },
+
+    overlay: (function () {
+        var _overlayDiv;
+        function getOverlayDiv() {
+            if (!_overlayDiv)
+                _overlayDiv = $('<div>').css({ position: 'absolute', top: '0', left: '0', 'z-index': '100', 'background-color': '#000', cursor: 'wait', filter: 'alpha(opacity=85)', '-moz-opacity': '0.85', opacity: '0.25', display: 'none' }).appendTo('body');
+            return _overlayDiv;
+        }
+
+        return {
+            show: function () { getOverlayDiv().setToSizeOfWindow().show(); return this; },
+            hide: function () { getOverlayDiv().hide(); return this; },
+            fadeIn: function (callback) { getOverlayDiv().setToSizeOfWindow().fadeIn(function () { (callback || function () { })(); }); return this; },
+            fadeOut: function (callback) { getOverlayDiv().fadeOut(function () { (callback || function () { })(); }); return this; }
+        };
+    })(),
+
+    post: function (url, data, callback) {
+        $.post(url, $.extend({}, { '__RequestVerificationToken': $('[name="__RequestVerificationToken"]').val() }, data), function (returnedData) {
+            if (typeof callback === "function") {
+                callback(returnedData);
+            }
+        });
+    },
+
+    Delayed: function (callback, millisecondsToDelay) {
+        /// <summary>
+        ///     Use this to delay executing some code for specified period of time.
+        ///     Useful in key events where you don't want events firing until there
+        ///     is a pause in the typing.
+        /// </summary>
+
+        if (!(this instanceof app.Delayed)) throw 'call new';
+
+        var timeout;
+
+        this.execute = function () {
+            window.clearTimeout(timeout);
+            var _this = this;
+            var args = arguments || [];
+            timeout = window.setTimeout(function () { callback.apply(_this, args); }, millisecondsToDelay || 500);
+        };
+
+        this.cancel = function () {
+            window.clearTimeout(timeout);
+        };
+    }
+};
+
+$(function() {
+
+    $('.smooth-scroll').click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 800);
+    });
+
+    (function () {
+        var width = $('<span>'),
+            height = $('<span>');
+
+        $('<p>')
+            .css({ 'font-size': '.85em' })
+            .append(width)
+            .append(' x ')
+            .append(height)
+            .append('  (320 x 421 - iphone)')
+            .appendTo($('footer'));
+
+        function setSize() {
+            width.text($(window).width());
+            height.text($(window).height());
+        }
+
+        setSize();
+        $(window).resize(setSize);
+    })();
+
+});
+
