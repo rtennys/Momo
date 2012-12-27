e38de6333c5d9fcf9ca9d678e0a41723cd68da96
Rob Tennyson <rob@robtennyson.us>
2012-12-27 12:08:49 -0600

    Added Facebook and Microsoft login providers

--- Momo.UI/App_Start/AuthConfig.cs
+++ Momo.UI/App_Start/AuthConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using DotNetOpenAuth.AspNet.Clients;
 using Microsoft.Web.WebPages.OAuth;
 
@@ -11,18 +12,12 @@ namespace Momo.UI
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
-
             //OAuthWebSecurity.RegisterTwitterClient(
             //    consumerKey: "",
             //    consumerSecret: "");
 
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
-
+            OAuthWebSecurity.RegisterFacebookClient(ConfigurationManager.AppSettings["FacebookAppId"], ConfigurationManager.AppSettings["FacebookAppSecret"]);
+            OAuthWebSecurity.RegisterMicrosoftClient(ConfigurationManager.AppSettings["MicrosoftClientId"], ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
             OAuthWebSecurity.RegisterGoogleClient();
             OAuthWebSecurity.RegisterClient(new OpenIdClient("MyOpenId", "https://myopenid.com/"), "MyOpenID", null);
             OAuthWebSecurity.RegisterYahooClient();
