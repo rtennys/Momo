f5087d9adaf95745da7d454fd9c1c3f203d32f99
rtennys <rob@robtennyson.us>
2015-08-29 16:00:59 -0500

    updated deployment

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,3 @@
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe deploy.proj /v:n
+"C:\Program Files (x86)\MSBuild\12.0\bin\msbuild.exe" deploy.proj /v:n
+
+@REM %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe deploy.proj /v:n
