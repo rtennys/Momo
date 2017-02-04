ac279182fd728f593f2d323677f655d58ec2ba65
Rob Tennyson <rob@robtennyson.us>
2017-02-04 07:41:21 -0600

    NuGet: jquery

--- Momo.UI/Scripts/_references.js
+++ Momo.UI/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-2.1.4.js" />
+﻿/// <reference path="jquery-3.1.1.js" />
 /// <reference path="Site.js" />
 /// <reference path="Site.AddListItem.Index.js" />
 /// <reference path="Site.ShoppingLists.Index.js" />
