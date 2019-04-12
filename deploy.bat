75b4c489ecf93b8db37fe70b96891c4e8f693365
Robert Tennyson <rob@robtennyson.us>
2019-04-12 08:57:36 -0500

    Updated deploy scripts and downgraded framework

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,5 @@
-"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" deploy.proj /v:n
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" deploy.proj /v:n
+
+@REM "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" deploy.proj /v:n
 
 @REM %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe deploy.proj /v:n
